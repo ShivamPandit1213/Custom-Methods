ad04464f5a263f71bfdf021122b4f857b2729ba6
Shivam Parashar <shivamparashar1213@gmail.com>
2025-10-10 19:42:56 +0530

    Update getChildWindowName.java

--- getChildWindowName.java
+++ getChildWindowName.java
@@ -13,3 +13,25 @@
 		}
 		return childName;
 	}
+
+_________________________________________________________________________________________
+
+		public WebDriver getWindowHandles(String parentWindowSessionId, String toClickOnLocator) throws Throwable {
+		WebElement elementTriggerWindow = driver.findElement(By.xpath(toClickOnLocator));
+		action.moveToElement(elementTriggerWindow);
+		elementTriggerWindow.click();
+
+		System.out.println("\nSwitch to new window - LogIn (Child Window)");
+		// new Window Launched - with Log-In Form
+		Set<String> sessionIds = driver.getWindowHandles();
+		wait.until(ExpectedConditions.numberOfWindowsToBe(2)); // Ensure new window has opened
+		System.out.println("Session Ids : " + sessionIds);
+
+		for (String windowHandle : driver.getWindowHandles()) {
+			if (!windowHandle.equals(parentWindowSessionId)) {
+				driver.switchTo().window(windowHandle);
+				System.out.println("Switched to new window with session id: " + windowHandle);
+			}
+		}
+		return driver;
+	}

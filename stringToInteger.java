cedbd45130a0585a1de1a1f90aa85d34bf3c65a3
Shivam Parashar <shivamparashar1213@gmail.com>
2025-10-12 12:11:10 +0530

    Update and rename textToInteger.java to stringToInteger.java

---
+++ stringToInteger.java
@@ -0,0 +1,12 @@
+	public int textToInteger(String text) {
+		int length = Integer.valueOf(text);
+		System.out.println("Final Integer: " + length);
+		return length;
+	}
+________________________________________________________
+
+		public int stringConvertToInteger(String stringToConvert) {
+		int convertedInteger = Integer.valueOf(stringToConvert);
+		System.out.println("\nAfter convert String into Integer : " + convertedInteger);
+		return convertedInteger;
+	}
--- textToInteger.java
+++
@@ -1,5 +0,0 @@
-	public int textToInteger(String text) {
-		int length = Integer.valueOf(text);
-		System.out.println("Final Integer: " + length);
-		return length;
-	}

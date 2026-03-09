f3c44a4a210263892fe006112b453eb9fd7e8429
Shivam Parashar <shivamparashar1213@gmail.com>
2026-03-09 22:03:57 +0530

    Add scrolling methods for WebDriver interactions

--- PublicMethod.java
+++ PublicMethod.java
@@ -67,6 +67,25 @@ public class PublicMethod {
 		return text;
 	}
 
+		public void scrollToTop(WebDriver driver) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Scroll to top
+		js.executeScript("window.scrollTo(0, 0);");
+	}
+
+	public void scrollToBottom(WebDriver driver) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Scroll to bottom
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+	}
+	
+	public void scrollToElement(String element) {
+		WebElement scroll = driver.findElement(By.xpath(element));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Scroll element into center
+		js.executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", scroll);
+	}
+	
 	public static WebElement waitForElementVisible(WebDriver driver, String locator) throws Exception {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		locatorWeb = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));

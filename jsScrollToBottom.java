2dd385da208840570dabe6b51dc31d7df552dfe7
Shivam Parashar <shivamparashar1213@gmail.com>
2026-04-24 09:23:53 +0530

    Rename method to jsScrollToBottom

--- jsScrollToBottom.java
+++ jsScrollToBottom.java
@@ -1,4 +1,4 @@
-public void scrollToBottom(WebDriver driver) {
+public void jsScrollToBottom(WebDriver driver) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		// Scroll to bottom
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
